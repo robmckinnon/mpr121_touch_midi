69ecb713ffce43336280f92a9f22ec6ef3996f37
robmckinnon <rob@movingflow>
2020-06-05 08:55:18 +0100

    Add midi aconnect implementation for raspbi zero usage

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-const midiBle = require("./midi-ble");
-
-let _characteristic = null;
-let _channel = null;
-const callback = {
-  controlChange: ({ controlNumber, value, channel }) => {
-    console.log({ controlNumber, value, channel });
-  },
-  noteOn: ({ note, velocity, channel }) => {
-    console.log({ note, velocity, channel });
-  },
-  noteOff: ({ note, channel }) => {
-    console.log({ note, channel });
-  },
-  setCharacteristic: (characteristic) => {
-    console.log('--- characteristic ---');
-    console.log(characteristic);
-    console.log('---');
-    _characteristic = characteristic;
-  },
-  setCharacteristicChannel: (uuid, channel) => {
-    console.log('--- channel ---');
-    console.log(uuid, channel);
-    console.log('---');
-    _channel = channel;
-  }
-};
-
-midiBle.initNoble(callback);
-
+const midi = require('midi');
 const MPR121 = require('adafruit-mpr121'),
       mpr121  = new MPR121(0x5A, 1);
 
+const activeSensing = 254;
+const noteoff = 8;
+const noteon = 9;
+const polyat = 10;
+const controlchange = 11;
+const programchange = 12;
+const channelat = 13;
+const pitchbend = 14;
+
+const output = new midi.Output();
+
+const count = output.getPortCount();
+
+// Get the name of a specified output port.
+console.log(output.getPortName(1));// eslint-disable-line
+
+// Open the first available output port.
+output.openPort(1);
+
+// Send a MIDI message.
+// output.sendMessage([176,22,1]);
+
+const noteOn = (channel, note, vel) => {
+  output.sendMessage([
+    channel & 0x0f | noteon,// eslint-disable-line
+    note,
+    vel]);
+}
+
+const noteOn = (channel, note, vel) => {
+  output.sendMessage([
+    channel & 0x0f | noteoff,// eslint-disable-line
+    note,
+    vel]);
+}
+
+// // Close the port when done.
+// output.closePort();
+
+const scale = [44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70]
+
 mpr121.on('touch', (pin) => {
-  console.log('.')
-  if (_characteristic && _channel) {
-    console.log('#')
-    const note = pin + 50;
-    midiBle.sendNoteOn(_characteristic, _channel, note, 97);
-  }
+  const note = scale[pin % 12];
+  noteOn(1, note);
 });
 
 mpr121.on('release', (pin) => {
-  if (_characteristic && _channel) {
-    console.log('-')
-    const note = pin + 50;
-    midiBle.sendNoteOff(_characteristic, _channel, note);
-  }
+  const note = scale[pin % 12];
+  noteOff(1, note);
 });
